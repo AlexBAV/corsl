0a51440faebecd5aad23f00dbed276bb4670933b
Alexander Bessonov <none@none>
2017-10-11 08:22:41 +0300

    Added missing file

---
+++ include/corsl/errors.h
@@ -0,0 +1,11 @@
+//-------------------------------------------------------------------------------------------------------
+// corsl - Coroutine Support Library
+// Copyright (C) 2017 HHD Software Ltd.
+// Written by Alexander Bessonov
+//
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "impl/errors.h"
